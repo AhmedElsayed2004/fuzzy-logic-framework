78a2e53d70c6cf6f221d847d6219b0ad6ae4844a
Ahmed Elsayed <ahmedelsayed12004@gmail.com>
2025-11-26 14:38:11 +0200

    feat: implement rule logic

---
+++ src/rules/RuleBaseEditor.java
@@ -0,0 +1,29 @@
+package rules;
+
+public class RuleBaseEditor {
+    RuleBase ruleBase;
+
+    public RuleBaseEditor() {
+        ruleBase = new RuleBase();
+    }
+
+    public void create(Rule rule) {
+        ruleBase.addRule(rule);
+    }
+
+    public void edit(int index, Rule rule) {
+        ruleBase.editRule(index, rule);
+    }
+
+    public void delete(int index) {
+        ruleBase.removeRule(index);
+    }
+
+    public void enable(int index) {
+        ruleBase.enableRule(index);
+    }
+
+    public void disable(int index) {
+        ruleBase.disableRule(index);
+    }
+}
---
+++ src/rules/RuleTerm.java
@@ -0,0 +1,21 @@
+package rules;
+
+import variables.LinguisticVariable;
+
+public class RuleTerm {
+    private final LinguisticVariable variable;
+    private final String setName; // the name of the fuzzy set inside the variable
+
+    public RuleTerm(LinguisticVariable variable, String setName) {
+        this.variable = variable;
+        this.setName = setName;
+    }
+
+    public double evaluate(double x) {
+        return variable.fuzzify(x).get(setName);
+    }
+
+    public String getSetName() {
+        return setName;
+    }
+}
